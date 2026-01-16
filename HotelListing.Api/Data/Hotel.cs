15e43561ba43c8c06e0da59a2fdbd013f62caad7
Gapos <gergana.apostolova.mail@gmail.com>
2026-01-16 09:54:44 +0200

    Add Country Class

--- HotelListing.Api/Data/Hotel.cs
+++ HotelListing.Api/Data/Hotel.cs
@@ -8,6 +8,8 @@
         public string Address { get; set; }
         public double Rating { get; set; }
 
+        public int CountryId { get; set; }
+        public Country? Country { get; set; }
 
     }
 }
