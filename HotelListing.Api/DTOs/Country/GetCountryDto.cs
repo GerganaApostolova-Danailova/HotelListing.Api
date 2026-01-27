4795e3c927679c38b36b0cf569e2d396e66159aa
Gapos <gergana.apostolova.mail@gmail.com>
2026-01-27 14:06:36 +0200

    Add DTOs to Hotel and Country

---
+++ HotelListing.Api/DTOs/Country/GetCountryDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelListing.Api.DTOs.Country
+{
+    public record GetCountryDto(
+        int Id,
+        string Name,
+        string ShortName
+    );
+
+    public record GetCountriesDto(
+        int Id,
+        string Name,
+        string ShortName
+    );
+}
