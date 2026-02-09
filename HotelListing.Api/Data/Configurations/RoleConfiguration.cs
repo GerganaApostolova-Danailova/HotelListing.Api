9bab132b62aaef3ca51f160410616c125f913768
Gapos <gergana.apostolova.mail@gmail.com>
2026-02-09 13:20:18 +0200

    Added DefaultRole

---
+++ HotelListing.Api/Data/Configurations/RoleConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.Api.Data.Configurations;
+
+public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+{
+    public void Configure(EntityTypeBuilder<IdentityRole> builder)
+    {
+        builder.HasData(
+            new IdentityRole { Id = "6bab5d5b-212f-4aba-a446-112043609d2f", Name = "Administrator", NormalizedName = "ADMINISTRATOR" },
+            new IdentityRole { Id = "c6a37278-57ba-4d2d-a4cd-8bb9a6a99c98", Name = "User", NormalizedName = "USER" }
+        );
+    }
+}
---
+++ HotelListing.Api/Migrations/20260209111955_AddedDefaultRole.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDefaultRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "6bab5d5b-212f-4aba-a446-112043609d2f", null, "Administrator", "ADMINISTRATOR" },
+                    { "c6a37278-57ba-4d2d-a4cd-8bb9a6a99c98", null, "User", "USER" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApiKeys_Key",
+                table: "ApiKeys",
+                column: "Key",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ApiKeys_Key",
+                table: "ApiKeys");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6bab5d5b-212f-4aba-a446-112043609d2f");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c6a37278-57ba-4d2d-a4cd-8bb9a6a99c98");
+        }
+    }
+}
