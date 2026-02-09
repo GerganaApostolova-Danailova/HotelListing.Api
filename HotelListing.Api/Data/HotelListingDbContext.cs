499f72b486e04f0ac74d88c6159dad7d5421c780
Gapos <gergana.apostolova.mail@gmail.com>
2026-02-09 13:12:18 +0200

    Remove Token

--- HotelListing.Api/Data/HotelListingDbContext.cs
+++ HotelListing.Api/Data/HotelListingDbContext.cs
@@ -13,6 +13,12 @@ public class HotelListingDbContext(DbContextOptions<HotelListingDbContext> optio
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        builder.Entity<ApiKey>(b=>
+        {
+            b.HasIndex(k => k.Key).IsUnique();
+        });
+
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
