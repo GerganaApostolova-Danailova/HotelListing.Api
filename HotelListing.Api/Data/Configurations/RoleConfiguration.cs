8d95e5d1551b4c7152178c97e968ec78b0249c91
Gapos <gergana.apostolova.mail@gmail.com>
2026-02-10 13:51:42 +0200

    Add Booking and HotelAdmin tables

--- HotelListing.Api/Data/Configurations/RoleConfiguration.cs
+++ HotelListing.Api/Data/Configurations/RoleConfiguration.cs
@@ -10,7 +10,9 @@ public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
     {
         builder.HasData(
             new IdentityRole { Id = "6bab5d5b-212f-4aba-a446-112043609d2f", Name = "Administrator", NormalizedName = "ADMINISTRATOR" },
-            new IdentityRole { Id = "c6a37278-57ba-4d2d-a4cd-8bb9a6a99c98", Name = "User", NormalizedName = "USER" }
+            new IdentityRole { Id = "c6a37278-57ba-4d2d-a4cd-8bb9a6a99c98", Name = "User", NormalizedName = "USER" },
+            new IdentityRole { Id = "c6a37278-a446-212f-a4cd-112043609d2f", Name = "Hotel Admin", NormalizedName = "HOTEL ADMIN" }
+
         );
     }
 }
--- HotelListing.Api/Data/HotelListingDbContext.cs
+++ HotelListing.Api/Data/HotelListingDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using System.Reflection.Emit;
 
 namespace HotelListing.Api.Data;
 
@@ -10,11 +11,23 @@ public class HotelListingDbContext(DbContextOptions<HotelListingDbContext> optio
     public DbSet<Hotel> Hotels { get; set; }
     public DbSet<ApiKey> ApiKeys { get; set; }
 
+    public DbSet<HotelAdmin> HotelAdmins { get; set; }
+
+    public DbSet<Booking> Bookings { get; set; }
+
+
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
-      
+        builder.Entity<Hotel>()
+       .Property(h => h.PerNightRate)
+       .HasPrecision(18, 2);
+
+        builder.Entity<Booking>()
+            .Property(b => b.TotalPrice)
+            .HasPrecision(18, 2);
 
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
